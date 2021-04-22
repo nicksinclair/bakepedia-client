914481ade9d357827284b2e606c6b68598040195
Nick Sinclair <nick.sinclair@itential.com>
2021-04-22 00:11:35 -0400

    Developed createPost logic in Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,10 @@
 // Absolute imports
 import React, { useState } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
 // Relative imports
+import { createPost } from "../../actions/posts";
 import useStyles from "./styles";
 
 const Form = () => {
@@ -14,15 +16,22 @@ const Form = () => {
     tags: "",
     selectedFiles: "",
   });
+  const dispatch = useDispatch();
 
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(createPost(postData));
+  };
+
+  const clear = () => {};
 
   return (
     <Paper className={classes.paper}>
       <form
-        autocomplete="off"
+        autoComplete="off"
         noValidate
-        clasSName={classes.form}
+        className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
         <Typography variant="h6">Share Recipe</Typography>
@@ -34,6 +43,55 @@ const Form = () => {
           value={postData.author}
           onChange={(e) => setPostData({ ...postData, author: e.target.value })}
         />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="body"
+          variant="outlined"
+          label="Body"
+          fullWidth
+          value={postData.body}
+          onChange={(e) => setPostData({ ...postData, body: e.target.value })}
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+
+        {/* <div className={classes.fileInput}>
+
+        </div> */}
+
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          size="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
       </form>
     </Paper>
   );
